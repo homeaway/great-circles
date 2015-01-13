73932f09b43a9bc114ebb10d6b12706b6defa956
bschneeman <bschneeman@homeaway.com>
2015-01-13 12:53:06 -0600

    moved the coordinates data frame directly into the GreatCircles call.

--- inst/examples/shiny-example.R
+++ inst/examples/shiny-example.R
@@ -2,18 +2,18 @@ library(shiny)
 library(greatCircles)
 library(htmlwidgets)
 
-coords = data.frame(longitude.start=c(138.6,145.0181,138.6,153.1693),
-                    latitude.start = c(-34.92862, -37.77931,-34.92862, -27.65875),
-                    longitude.finish = c(-73.928164, -73.946490, 2.336655, -73.919071),
-                    latitude.finish = c(40.76352, 40.78027, 48.88156, 40.75708)
-                    )
-
 ui = shinyUI(fluidPage(
-  greatCirclesOutput('arcs')
+  greatCirclesOutput("arcs")
 ))
 
 server = function(input, output) {
-  output$arcs <- rendergreatCircles(coords)
+  output$arcs <- rendergreatCircles(
+    data.frame(longitude.start=c(138.6,145.0181,138.6,153.1693),
+               latitude.start = c(-34.92862, -37.77931,-34.92862, -27.65875),
+               longitude.finish = c(-73.928164, -73.946490, 2.336655, -73.919071),
+               latitude.finish = c(40.76352, 40.78027, 48.88156, 40.75708)
+    )
+  )
 }
 
 shinyApp(ui = ui, server = server)
--- inst/examples/standalone-example.R
+++ inst/examples/standalone-example.R
@@ -1,8 +1,8 @@
 library(greatCircles)
 
-coords = data.frame(longitude.start=c(138.6,145.0181,138.6,153.1693),
-                    latitude.start = c(-34.92862, -37.77931,-34.92862, -27.65875),
-                    longitude.finish = c(-73.928164, -73.946490, 2.336655, -73.919071),
-                    latitude.finish = c(40.76352, 40.78027, 48.88156, 40.75708)
+greatCircles(data.frame(longitude.start=c(138.6,145.0181,138.6,153.1693),
+                        latitude.start = c(-34.92862, -37.77931,-34.92862, -27.65875),
+                        longitude.finish = c(-73.928164, -73.946490, 2.336655, -73.919071),
+                        latitude.finish = c(40.76352, 40.78027, 48.88156, 40.75708)
+                        )
 )
-greatCircles(coords)
