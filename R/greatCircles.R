1e3a6d3467aa6360674c858b024215be22b8a1da
bschneeman <bschneeman@homeaway.com>
2015-01-13 09:52:16 -0600

    Initial Commit

---
+++ R/greatCircles.R
@@ -0,0 +1,51 @@
+#' Great Circles
+#'
+#' Draws Great Circle arcs in an svg container using D3 Projections
+#'
+#' @import htmlwidgets
+#'
+#' @export
+greatCircles <- function(coordinates, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    coords = lapply(1:nrow(coordinates), toCoordsLocal, coordinates)
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'greatCircles',
+    x,
+    width = width,
+    height = height,
+    package = 'greatCircles'
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+greatCirclesOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'greatCircles', width, height, package = 'greatCircles')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+rendergreatCircles <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, greatCirclesOutput, env, quoted = TRUE)
+}
+
+#' Utility function to convert from the data.frame 
+#' to a list for javascript serialization
+#' 
+toCoordsLocal <- function(i,data) {
+  list(type="LineString",
+       coordinates=list(
+         c(data[i,"longitude.start"], data[i,"latitude.start"]), 
+         c(data[i,"longitude.finish"], data[i,"latitude.finish"])
+       )
+  )
+}
+
---
+++ inst/htmlwidgets/greatCircles.js
@@ -0,0 +1,71 @@
+HTMLWidgets.widget({
+
+  name: 'greatCircles',
+
+  type: 'output',
+
+  initialize: function(el, width, height) {
+
+    var width = window.innerWidth * 0.9, //960,
+    height = width/2;
+
+//select(el).
+    d3.select(".greatCircles").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+
+    return {
+      // TODO: add instance fields as required
+
+      projection : d3.geo.equirectangular()
+                    .scale(100*width/640)
+                    .translate([width / 2, height / 2])
+                    .precision(.1)
+    };
+
+  },
+
+  renderValue: function(el, x, instance) {
+
+    var path = d3.geo.path()
+    .projection(instance.projection);
+
+    //x.coords contains the list of coordinates that will be transformed
+    //into great circle arcs
+    arcs = x.coords;
+
+    // select the svg element and remove existing childern
+    //.select(el)
+    var svg = d3.select(".greatCircles").select("svg");
+    svg.selectAll("*").remove();
+
+    // this is where we draw the trip-related arcs
+    svg.selectAll("svg")
+      .data(arcs)
+      .enter()
+      .append("path")
+      .attr("class", "arc")
+      .attr("d", path);
+
+    var shape = HTMLWidgets.getAttachmentUrl('shapes', 'world')
+
+    d3.json(shape, function(error, world) {
+
+      svg.insert("path", ".arc")
+          .datum(topojson.feature(world, world.objects.land))
+          .attr("class", "land")
+          .attr("d", path);
+
+      svg.insert("path", ".arc")
+          .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+          .attr("class", "boundary")
+          .attr("d", path);
+
+  });
+},
+  resize: function(el, width, height, instance) {
+
+  }
+
+});
